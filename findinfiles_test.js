0db69cac1489546eaf78dba4482de4c22f233140
Fabian Jakobs <fabian@c9.io>
2013-09-30 09:03:43 +0000

    don't pass along the smith.io config. Instead pass the list of vfs servers

--- findinfiles_test.js
+++ findinfiles_test.js
@@ -58,12 +58,9 @@ require(["lib/architect/architect", "lib/chai/chai", "/vfs-root"],
         "plugins/c9.ide.keys/commands",
         "plugins/c9.fs/proc",
         {
-            packagePath: "plugins/c9.vfs.client/vfs_client",
-            smithIo     : {
-                "path": "/smith.io/server"
-            }
+            packagePath: "plugins/c9.vfs.client/vfs_client"
         },
-        "plugins/c9.vfs.client/endpoint.standalone",
+        "plugins/c9.vfs.client/endpoint",
         "plugins/c9.ide.auth/auth",
         "plugins/c9.fs/fs",
         
