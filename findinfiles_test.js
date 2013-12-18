e41e41464d309b75853c244d4ee9163998e68a79
Mostafa Eweda <mostafa@c9.io>
2013-12-18 13:52:53 +0100

    Better error handler + refactor c9.ide.api/api to c9.core/api

--- findinfiles_test.js
+++ findinfiles_test.js
@@ -28,7 +28,7 @@ require(["lib/architect/architect", "lib/chai/chai", "/vfs-root"],
             packagePath : "plugins/c9.core/settings",
             settings    : "<settings><user><general animateui='true' /></user></settings>"
         },
-        "plugins/c9.ide.api/api.js",
+        "plugins/c9.core/api.js",
         {
             packagePath  : "plugins/c9.ide.ui/ui",
             staticPrefix : "plugins/c9.ide.ui"
